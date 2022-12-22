4bc4c36d926d0b26b34cb4e6f7fa73660a3e09a3
Sandro Cavallari <sandro.cavallari@zalando.de>
2022-12-22 17:59:47 +0100

    add weight decay patters support on regex

--- semantic_retrieval/optim/optimizers.py
+++ semantic_retrieval/optim/optimizers.py
@@ -1,33 +1,36 @@
 import torch
+import re
 from typing import List, Tuple, Optional
 
-from semantic_retrieval.optim import Adam
+from semantic_retrieval.optim.adam import Adam
 
 
 def get_group_params(
     named_parameters: List[Tuple[str, torch.nn.Parameter]],
     weight_decay: float,
-    no_decay: Optional[List[str]] = None,
+    no_decay_patterns: Optional[List[str]] = None,
 ):
     """
     package the parameters in 2 groups for proper weight decay
     :param named_parameters: named parameters list
     :param weight_decay: weight decay to use
-    :param no_decay: list of parameter with no decay
+    :param no_decay_patterns: list of parameter with no decay
     :return:
     """
     optimizer_grouped_parameters = [
         dict(
             params=[
-                p for n, p in named_parameters
-                if not any(nd in n for nd in no_decay)
+                p for n, p in named_parameters if not any(
+                    [re.search(pattern, n) for pattern in no_decay_patterns]
+                )
             ],
             weight_decay=weight_decay,
         ),
         dict(
             params=[
-                p for n, p in named_parameters
-                if any(nd in n for nd in no_decay)
+                p for n, p in named_parameters if any(
+                    [re.search(pattern, n) for pattern in no_decay_patterns]
+                )
             ],
             weight_decay=0.,
         )
