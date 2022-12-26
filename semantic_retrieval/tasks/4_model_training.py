0f597c5d43c5c7ebc22706c20826637d2454f427
Sandro Cavallari <sandro.cavallari@zalando.de>
2022-12-26 23:39:33 +0100

    update parameters for remote execution

--- semantic_retrieval/tasks/4_model_training.py
+++ semantic_retrieval/tasks/4_model_training.py
@@ -48,7 +48,7 @@ class FarFetchModelTraining(Task):
         parser.add_argument(
             "--device",
             type=str,
-            default="mps",
+            default="cuda",
         )
 
         parser.add_argument(
@@ -60,34 +60,35 @@ class FarFetchModelTraining(Task):
         parser.add_argument(
             "--train_batch_size",
             type=int,
-            default=32,
+            default=128,
         )
 
         parser.add_argument(
             "--eval_batch_size",
             type=int,
-            default=1000,
+            default=500,
         )
 
         parser.add_argument(
             "--batches_per_epoch",
             type=int,
-            default=10000,
+            default=2300,
         )
 
-        parser.add_argument("--gradient_accumulation_steps",
-                            default=1,
-                            type=int)
-        parser.add_argument("--freeze_layer", default=3, type=int)
+        parser.add_argument(
+            "--gradient_accumulation_steps",
+            default=1,
+            type=int,
+        )
         parser.add_argument("--optim_method", default="adam")
         parser.add_argument("--weight_decay", default=0.01, type=float)
         parser.add_argument("--lr", default=5e-5, type=float)
         parser.add_argument("--max_grad_norm", default=1.0, type=float)
         parser.add_argument("--n_gpus", default=1, type=int)
-        parser.add_argument("--eval_every", default=5, type=int)
-
-        args = compute_warmup_steps(args)
+        parser.add_argument("--eval_every", default=1, type=int)
+        parser.add_argument("--num_workers", default=1, type=int)
         args, _ = parser.parse_known_args()
+        args = compute_warmup_steps(args)
 
         print("\n\n-----------")
         for k, v in vars(args).items():
@@ -106,19 +107,19 @@ class FarFetchModelTraining(Task):
         checkpoint_path = Path(self.args.checkpoint_base_path)
 
         model_path = checkpoint_path.joinpath(
-            "pre-traiend",
+            "pre-trained",
             "ViT-L-14@336px.pt",
         )
-
-
         device = torch.device(self.args.device)
+        # device = torch.device("cpu")
 
         state_dict = torch.load(
-            "/dbfs" + model_path.as_posix(),
+            model_path.as_posix(),
             map_location="cpu",
         )
-
-        model = CLIP(**LargeClipConfig()).load_state_dict(
+        model_config = LargeClipConfig()
+        model = CLIP(**model_config.to_dict())
+        model.load_state_dict(
             state_dict,
             strict=True,
         )
@@ -132,8 +133,8 @@ class FarFetchModelTraining(Task):
         )
         unfreeze_layer_params(
             named_params,
-            img_layer=6,
-            text_layer=3,
+            img_layer=15,
+            text_layer=8,
         )
         optim = get_optimizer(
             method=self.args.optim_method,
@@ -156,27 +157,32 @@ class FarFetchModelTraining(Task):
             args=self.args,
         )
 
-        
         experimet = comet_ml.Experiment(
             project_name="clip",
             auto_output_logging="simple",
+            log_env_details=True,
+            log_env_gpu=True,
+            log_env_cpu=True,
+            log_env_host=True,
         )
 
         experimet.set_name(self.args.run_name)
         experimet.add_tag("farfetch-large")
         experimet.log_parameters(vars(self.args))
 
-
         best_f1 = 0.0
-        train_metrics = []
-        eval_metrics = []
-        
+
         try:
             for epoch in range(1, self.args.epochs + 1):
                 experimet.set_epoch(epoch)
+
                 with get_farfetch_dataloader(
                         path="file:///dbfs" +
-                        train_dataset_path.as_posix()
+                        train_dataset_path.as_posix(),
+                        # path="file://" + train_dataset_path.absolute().as_posix(),
+                        batch_size=self.args.train_batch_size,
+                        reader_pool_type="process",
+                        workers_count=self.args.num_workers,
                 ) as train_dl:
                     train_metric = task.train(
                         model=model,
@@ -187,58 +193,59 @@ class FarFetchModelTraining(Task):
                     )
 
                     experimet.log_metrics(train_metric, epoch=epoch)
-                    train_metrics.append(train_metric)
 
                     print(
-                        "epoch:{epoch} \t acc-img:{acc_img} \t acc-text:{acc_text} \t loss:{loss}".format(
+                        "epoch:{epoch} \t acc-img:{acc_img} \t acc-text:{acc_text} \t loss:{loss}"
+                        .format(
                             epoch=epoch,
                             acc_img=train_metric["train_accuracy_img"],
                             acc_text=train_metric["train_accuracy_text"],
                             loss=train_metric["train_loss"],
-                        )
-                    )
+                        ))
 
                 if epoch % self.args.eval_every == 0 or epoch == 1:
 
-                with get_farfetch_dataloader(
-                        path="file:///dbfs" +
-                        validation_dataset_path.as_posix()
-                ) as eval_dl:
-                    is_best = False
-                    eval_metric = task.eval(
-                        model=model,
-                        dataloader=train_dl,
-                        device=device,
-                    )
-
-                    if eval_metric["eval_f_score_text"] > best_f1:
-                        best_f1 = eval_metric["eval_f_score_text"]
-                        is_best = True
-                    
-
-                    if isinstance(model, torch.nn.DataParallel):
-                        state_dict = dict(
-                            [(n, p.to("cpu")) for n, p in model.module.state_dict().items()]
-                        )
-                    else:
-                        state_dict = dict(
-                            [(n, p.to("cpu")) for n, p in model.state_dict().items()]
+                    with get_farfetch_dataloader(
+                            path="file://dbfs/" + validation_dataset_path.as_posix(),
+                            batch_size=self.args.eval_batch_size,
+                            reader_pool_type="process",
+                            workers_count=self.args.num_workers,
+                    ) as eval_dl:
+                        is_best = False
+                        eval_metric = task.eval(
+                            model=model,
+                            dataloader=eval_dl,
+                            device=device,
                         )
 
-                    save_checkpoint(
-                        path_=checkpoint_path.joinpath(
-                            "clip",
-                            "farfetch",
-                        ),
-                        state=state_dict,
-                        is_best=is_best,
-                        filename=f"ckp_{epoch}.pt",
-                    )
+                        if eval_metric["eval_f_score_text"] > best_f1:
+                            best_f1 = eval_metric["eval_f_score_text"]
+                            is_best = True
+
+                        if isinstance(model, torch.nn.DataParallel):
+                            state_dict = dict([
+                                (n, p.to("cpu"))
+                                for n, p in model.module.state_dict().items()
+                            ])
+                        else:
+                            state_dict = dict([
+                                (n, p.to("cpu"))
+                                for n, p in model.state_dict().items()
+                            ])
+
+                        save_checkpoint(
+                            path_=checkpoint_path.joinpath(
+                                "clip",
+                                "farfetch",
+                            ),
+                            state=state_dict,
+                            is_best=is_best,
+                            filename=f"ckp_{epoch}.pt",
+                        )
         finally:
             experimet.end()
 
 
-
 if __name__ == '__main__':
-    task = FarFetchDatasetPreparation()
+    task = FarFetchModelTraining()
     task.launch()
