fff305a807bb53ee96b1692eb88229d6590d5f65
Sandro Cavallari <sandro.cavallari@zalando.de>
2022-12-26 23:17:03 +0100

    impoove parameter frezer

--- semantic_retrieval/optim/param_freezers.py
+++ semantic_retrieval/optim/param_freezers.py
@@ -18,29 +18,33 @@ def unfreeze_layer_params(
             p.requires_grad = True
             print(f"unfreeze -> {n}")
 
-        elif n.startswith("text."):
-
-            if n.startswith("text.resblocks."):
-                layer_num = n.split(".")[2]
-                if layer_num >= text_layer:
-                    p.requires_grad = True
-                    print(f"unfreeze -> {n}")
-
-            if n.startswith("text.ln_final"):
+        elif n.startswith("text.resblocks."):
+            layer_num = int(n.split(".")[2])
+            if layer_num >= text_layer:
                 p.requires_grad = True
                 print(f"unfreeze -> {n}")
 
-        elif n.startswith("vision."):
+            else:
+                p.requires_grad = False
+                print(f"FREEZE -> {n}")
 
-            if n.startswith("vision.resblocks."):
-                layer_num = n.split(".")[2]
-                if layer_num >= img_layer:
-                    p.requires_grad = True
-                    print(f"unfreeze -> {n}")
+        elif n.startswith("text.ln_final"):
+            p.requires_grad = True
+            print(f"unfreeze -> {n}")
 
-            if n.startswith("vision.ln_post"):
+        elif n.startswith("vision.resblocks."):
+            layer_num = int(n.split(".")[2])
+            if layer_num >= img_layer:
                 p.requires_grad = True
                 print(f"unfreeze -> {n}")
+
+            else:
+                p.requires_grad = False
+                print(f"FREEZE -> {n}")
+
+        elif n.startswith("vision.ln_post"):
+            p.requires_grad = True
+            print(f"unfreeze -> {n}")
 
         else:
             print(f"FREEZE -> {n}")
