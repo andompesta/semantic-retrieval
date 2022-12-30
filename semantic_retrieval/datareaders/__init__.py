15165c2f1d816adda3cacaab4e06e36d320f9c8e
Sandro Cavallari <sandro.cavallari@zalando.de>
2022-12-30 11:10:38 +0100

    add single batch reader for debugging

--- semantic_retrieval/datareaders/__init__.py
+++ semantic_retrieval/datareaders/__init__.py
@@ -1,5 +1,9 @@
-from .farfetch_datareader import get_farfetch_dataloader
+from .farfetch_datareader import (
+    get_farfetch_dataloader,
+    get_single_batch_farfetch_dataloader,
+)
 
 __all__ = [
     "get_farfetch_dataloader",
+    "get_single_batch_farfetch_dataloader",
 ]
--- semantic_retrieval/datareaders/farfetch_datareader.py
+++ semantic_retrieval/datareaders/farfetch_datareader.py
@@ -16,7 +16,7 @@ def get_farfetch_dataloader(
             reader_pool_type=reader_pool_type,
             workers_count=workers_count,
             num_epochs=1,
-            shuffle_rows=shuffle,
+            # shuffle_rows=shuffle,
             shuffle_row_groups=shuffle,
     ) as reader:
         dataloader = DataLoader(
@@ -24,3 +24,29 @@ def get_farfetch_dataloader(
             batch_size=batch_size,
         )
         yield dataloader
+
+
+@contextmanager
+def get_single_batch_farfetch_dataloader(
+    path: str,
+    batch_size: int,
+    reader_pool_type: str,
+    workers_count: int = 5,
+    shuffle: bool = True,
+):
+    with make_reader(
+            path,
+            reader_pool_type=reader_pool_type,
+            workers_count=workers_count,
+            num_epochs=1,
+            # shuffle_rows=shuffle,
+            shuffle_row_groups=shuffle,
+    ) as reader:
+        dataloader = DataLoader(
+            reader,
+            batch_size=batch_size,
+        )
+        for batch in dataloader:
+            break
+
+    yield [batch]
